13bd14017e807485081ca6280681ba169cb38fc7
NohJeongKim <ajouunihospitalrn@naver.com>
2022-07-06 12:34:23 +0900

    혼자서 다시 count 세어보기

--- js/start.js
+++ js/start.js
@@ -7,65 +7,70 @@ const select = [];
 
 function calResult() {
   const setArray = [
-    { name: "비빔냉면", value: 0, key: 0 },
-    { name: "회냉면", value: 0, key: 1 },
-    { name: "라면", value: 0, key: 2 },
-    { name: "홍어무침", value: 0, key: 3 },
-    { name: "메밀쫄면", value: 0, key: 4 },
-    { name: "제육덮밥", value: 0, key: 5 },
-    { name: "물냉면", value: 0, key: 6 },
-    { name: "물만두", value: 0, key: 7 },
-    { name: "왕만두", value: 0, key: 8 },
-    { name: "왕만두튀김", value: 0, key: 9 },
-    { name: "메밀온면", value: 0, key: 10 },
-    { name: "한우만두국", value: 0, key: 11 },
-    { name: "이박사김밥", value: 0, key: 12 },
-    { name: "등심돈까스", value: 0, key: 13 },
-    { name: "치즈돈까스", value: 0, key: 14 },
-    { name: "생선까스", value: 0, key: 15 },
-    { name: "메밀콩국수", value: 0, key: 16 },
+    { id: 0, name: "비빔냉면", count: 0 },
+    { id: 1, name: "회냉면", count: 0 },
+    { id: 2, name: "라면", count: 0 },
+    { id: 3, name: "홍어무침", count: 0 },
+    { id: 4, name: "메밀쫄면", count: 0 },
+    { id: 5, name: "제육덮밥", count: 0 },
+    { id: 6, name: "물냉면", count: 0 },
+    { id: 7, name: "물만두", count: 0 },
+    { id: 8, name: "왕만두", count: 0 },
+    { id: 9, name: "왕만두튀김", count: 0 },
+    { id: 10, name: "메밀온면", count: 0 },
+    { id: 11, name: "한우만두국", count: 0 },
+    { id: 12, name: "등심돈까스", count: 0 },
+    { id: 13, name: "치즈돈까스", count: 0 },
+    { id: 14, name: "생선까스", count: 0 },
+    { id: 15, name: "메밀콩국수", count: 0 },
+    { id: 16, name: "이박사김밥", count: 0 },
   ];
 
+  /* 주의해야 되는 부분.... 헷갈리니까 열심히 생각해보기. */
+
   for (let i = 0; i < countOfData; i++) {
-    // console.log(i);
+    // i = 0~9
     const target = qnaList[i].a[select[i]];
+    // console.log(target); 내가 고른 답들이 모두 나타났다.
+    // 무조건 name 1개씩 끌고 들어오기. 그래야 비교가 가능하다. (주의하기.)
     for (let j = 0; j < target.type.length; j++) {
+      // j = 0~9
+      // target 모두 펼치기.
       for (let k = 0; k < setArray.length; k++) {
+        // k = 0~16
+        // setArray 모두 펼치기.
         if (target.type[j] === setArray[k].name) {
-          setArray[k].value += 1; // 몇 개 나왔는지 개수를 세어주기.
+          setArray[k].count += 1;
         }
       }
     }
   }
 
+  /* 이 부분도 이해가 잘 가지 않는다. */
+  /* 단어를 고려해서 count의 수를 세어주고 -> 내림차순을 이용해서 가장 많이 나온 단어의 id를 가져온다. */
   const resultArray = setArray.sort(function (a, b) {
-    // 오름차순 ?? 이해가 안 간다.
-    if (a.value > b.value) {
-      return -1;
-    }
-    if (a.value < b.value) {
-      return 1;
-    }
-    return 0;
+    return b.count - a.count; // 내림차순
   });
-  console.log(resultArray);
-  let resultWord = resultArray[0].key;
-  return resultWord;
+
+  // console.log(resultArray);
+  let result = resultArray[0].id;
+  // 가장 count 수가 많은 음식의 id를 의미한다.
+  // console.log(result);
 }
 
 function goResult() {
-  qna.style.webkitAnimation = "fadeOut 1s";
   qna.style.animation = "fadeOut 1s";
+  qna.style.webkitAnimation = "fadeOut 1s";
   setTimeout(() => {
-    result.style.webkitAnimation = "fadeIn 1s";
     result.style.animation = "fadeIn 1s";
+    result.style.webkitAnimation = "fadeIn 1s";
     setTimeout(() => {
       result.style.display = "block";
       qna.style.display = "none";
     }, 450);
   }, 450);
 
-  // console.log(select); answer 결과를 잘 가져올 수 있다.
+  // console.log(select); 제대로 담겨있음을 알 수 있다.
   calResult();
 }
 
@@ -98,16 +103,16 @@ function addAnswer(answerText, qIdx, idx) {
       }
       goNext(++qIdx);
     }, 450);
-    // console.log(idx);
   });
 }
 
 function goNext(qIdx) {
-  if (qIdx == countOfData) {
-    goResult();
-    return; // Error q 사라지게 하기 위함이다.
-  }
   // "시작하기 버튼을 누르면 뒤로 넘어가기."
+  if (qIdx === countOfData) {
+    return goResult();
+    // goResult();
+    // return; // 함수가 더 실행되지 않도록 만들어주는 역활
+  }
   const q = document.querySelector("#qnaB");
   const statusBar = document.querySelector(".statusBar");
   statusBar.style.width = (100 / countOfData) * (qIdx + 1) + "%";
@@ -126,6 +131,8 @@ function goNext(qIdx) {
     addAnswer(qnaList[qIdx].a[i].answer, qIdx, i); // 꼭 넘겨주고 이용해야된다.
     // 계속 넘기고 넘겨야 된다.
   }
+
+  qIdx += 1;
 }
 
 function start() {
